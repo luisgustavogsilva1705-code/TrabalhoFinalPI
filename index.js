585df0781702ecaa4826e80b49e9a1d1bec4a24f
Luis Gustavo <luisgustavogsilca1705@gmai.com>
2025-12-07 15:56:08 -0300

    adicionando validação no pagina de cadastro de equipes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 5000;
 
 const server = express();
 var usuarioLogin = false;
+var lista_Equipe =[];
 
 server.use(session({
     secret: "Acess0S3cre70",
@@ -49,10 +50,6 @@ server.get("/",(requsicao, resposta)=>{
                 <div class="mb-3">
                     <label for="Senha" class="form-label">Password</label>
                     <input type="password" name="loginSenha" class="form-control" id="Senha">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -97,10 +94,6 @@ server.post("/",(requisicao, resposta)=>{
                     <div class="mb-3">
                         <label for="Senha" class="form-label">Password</label>
                         <input type="password" name="loginSenha" class="form-control" id="Senha">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
 
                     <div>
@@ -168,7 +161,7 @@ server.get("/cadastroEquipes",(requisicao, resposta)=>{
                 <h3 style="text-align: center; text-decoration:underline; color: red;" >Cadastro de Equipes E-Sportes</h3>
                 <div style="display:flex; justify-content:center; align-items:center; height:100vh;">
                 
-                    <form action="/listaEquipes" method="POST" style="width:300px; width:300px; padding:20px; border:2px solid #444; border-radius:10px;">
+                    <form action="/cadastroEquipes" method="POST" style="width:300px; width:300px; padding:20px; border:2px solid #444; border-radius:10px;">
 
                         <label style="display:block; margin-bottom:5px; font-weight:bold;">Nome da Equipe:</label>
                         <input type="text" name="nomeEquipe" style="width:100%; padding:10px; margin-bottom:15px; border-radius:5px; border:1px solid #ccc;">
@@ -182,6 +175,11 @@ server.get("/cadastroEquipes",(requisicao, resposta)=>{
                         <button type="submit" style="width:100%; padding:12px; background:#007bff; color:white; border:none; border-radius:5px; font-size:16px; cursor:pointer;">
                             Enviar
                         </button>
+                        <br>
+                        <br>
+                        <a href="/menu" style="display:block; text-align:center; width:100%; padding:12px; background:#6c757d; color:white; border-radius:5px; font-size:16px; text-decoration:none; cursor:pointer;">
+                            Voltar
+                        </a>
 
                     </form>
                 </div>
@@ -189,6 +187,93 @@ server.get("/cadastroEquipes",(requisicao, resposta)=>{
         </html>
         `
     );
+});
+
+server.post('/cadastroEquipes',(requisicao, resposta)=>{
+
+    const nomeEquipe= requisicao.body.nomeEquipe;
+    const nomeCapitao= requisicao.body.nomeCapitao;
+    const contato= requisicao.body.contato;
+
+    if(nomeEquipe && nomeCapitao && contato)
+    {
+        lista_Equipe.push({nomeEquipe,nomeCapitao, contato});
+        resposta.redirect("/listaEquipes");
+    }
+    else
+    {
+        let cont=`
+            <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Login</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                </head>
+            <body>
+                <h3 style="text-align: center; text-decoration:underline; color: red;" >Cadastro de Equipes E-Sportes</h3>
+                <div style="display:flex; justify-content:center; align-items:center; height:100vh;">
+                
+                    <form action="/cadastroEquipes" method="POST" style="width:300px; width:300px; padding:20px; border:2px solid #444; border-radius:10px;">
+
+                        <label style="display:block; margin-bottom:5px; font-weight:bold;">Nome da Equipe:</label>
+                        <input type="text" value="${nomeEquipe}" name="nomeEquipe" style="width:100%; padding:10px; margin-bottom:15px; border-radius:5px; border:1px solid #ccc;">`
+        if(!nomeEquipe)
+        {
+            cont+=`
+                <div>
+                    <p style="color: red" >Deve-se preencher esse campo</p>
+                </div>
+            `
+        }
+        cont+=`
+                        <label style="display:block; margin-bottom:5px; font-weight:bold;">Nome do Capitão:</label>
+                        <input type="text" value="${nomeCapitao}" name="nomeCapitao" style="width:100%; padding:10px; margin-bottom:15px; border-radius:5px; border:1px solid #ccc;">`
+        if(!nomeCapitao)
+        {
+            cont+=`
+            <div>
+                <p style="color:red" >Deve-se preencher esse campo</p>
+            </div>
+            `
+        }
+        cont+=`
+                        <label style="display:block; margin-bottom:5px; font-weight:bold;">Contato (WhatsApp):</label>
+                        <input type="text" value="${contato}" name="contato" style="width:100%; padding:10px; margin-bottom:20px; border-radius:5px; border:1px solid #ccc;">`
+        if(!contato)
+        {
+            cont+=`
+            <div>
+                <p style="color:red" >Deve-se preencher esse campo</p>
+            </div>
+            `
+        }
+        cont+=`
+                        <button type="submit" style="width:100%; padding:12px; background:#007bff; color:white; border:none; border-radius:5px; font-size:16px; cursor:pointer;">
+                            Enviar
+                        </button>   
+                        <br>
+                        <br>
+                        <a href="/menu" style="display:block; text-align:center; width:100%; padding:12px; background:#6c757d; color:white; border-radius:5px; font-size:16px; text-decoration:none; cursor:pointer;">
+                            Voltar
+                        </a>
+
+                    </form>
+                </div>
+            </body>
+        </html>
+        `
+        resposta.send(cont);
+    }
+
+});
+
+server.get("/listaEquipes",(requisicao, resposta)=>{
+
+    resposta.send(`
+        
+        `
+    )
 });
 
 
